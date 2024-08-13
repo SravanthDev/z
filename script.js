f2d98db1dda05bde488c6a89ab577e43a90c9129
TechMartial <sravanth7762@gmail.com>
2024-08-13 12:46:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById('saveContact').addEventListener('click', function() {
     const link = document.createElement('a');
-    link.href = ''; // Replace with the actual path to your .vcf file
-    link.download = '.vcf';
+    link.href = 'onestopsolutions_1234567890.vcf'; // Replace with the actual path to your .vcf file
+    link.download = 'onestopsolutions_1234567890.vcf';
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
