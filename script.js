3d9ea4fd55f764e62fa476f5cbc6ae355fbece1e
TechMartial <sravanth7762@gmail.com>
2024-08-13 12:07:37 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+document.getElementById('saveContact').addEventListener('click', function() {
+    const vCardData = `
+BEGIN:VCARD
+VERSION:3.0
+FN:John Doe
+ORG:Your Company
+TEL:+1234567890
+EMAIL:john.doe@example.com
+END:VCARD
+    `;
+    
+    const blob = new Blob([vCardData], { type: 'text/vcard' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'contact.vcf';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+});
